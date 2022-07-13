dedfe75f9bfb5c1965763bc878a917d0a90582c0
Fasieh111 <108819341+Fasieh111@users.noreply.github.com>
2022-07-13 21:33:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Best Movie In The World</h1>
+<p1>There is no one "best" movie in the world. However, there are many great movies that can be enjoyed by people of all ages and backgrounds. Some of the best movies ever made include "Avengers Endgame" and "Avatar" These films are all well-made, entertaining, and memorable. They have stood the test of time, and continue to be popular with audiences around the world.</p1>
+<br>
+<br>
+<img
+      src="https://moviewarden.files.wordpress.com/2019/05/endgame-vs.-avatar.jpg?w=798"/>
+
+<h2>Which movie has made the most money?</h2>
+<p2>Avatar was the highest grossing movie of all time when it was released in 2009. However, Avengers: Endgame dethroned Avatar as the top-grossing movie in 2019. Surprisingly, Avatar reclaimed the number one spot in 2021. There are several reasons why Avatar has been so successful over the years.</p2>
+<br>
+<br>
+<p3>The visually stunning world of Pandora is one of the main reasons for Avatar’s success. The setting is incredibly unique and mesmerizing, and viewers can’t help but be drawn into this fantastical place. The characters are also well-developed and interesting, which helps keep audiences engaged throughout the film. Additionally, James Cameron’s direction is impeccable; he creates an immersive experience that sucks viewers into Pandora and never lets them go until the end credits roll.<p3>
+<br>
+<br>
+<p4>Although Avengers: Endgame surpassed Avatar at the box office initially, it seems that audiences have ultimately decided that they prefer Cameron’s epic adventure over Marvel Studios’ blockbuster spectacle. With its breathtaking visuals, emotionally resonant story, and masterful direction, it’s easy to see why Avatar continues to reign supreme as Hollywood's top dog.</p4>
+
+
+`;
